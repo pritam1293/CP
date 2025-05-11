a2e9ef8c3ad2f02884987f7443403506146f9ac4
pritam1293 <beherapritam2003@gmail.com>
2025-05-11 12:14:14 +0530

    As of may 11, 2025

--- code.cpp
+++ code.cpp
@@ -8,63 +8,10 @@ int main() {
     freopen("output.txt", "w", stdout);
 #endif
     int t=1;
-    // cin>>t;
     while(t--)  solve();
     cerr << "time: " << (float)clock() / CLOCKS_PER_SEC << endl; return 0;
 }
 
-pair<int,int> good(int n, int d, vector<int>& a, double M) {
-    vector<double> pre(n+1), pmin(n+1);
-    vector<int> pminidx(n+1);
-
-    pre[0] = a[0] - M;
-    pmin[0] = pre[0];
-    pminidx[0] = 0;
-    for (int i = 1; i <= n; i++) {
-        pre[i] = pre[i-1] + a[i] - M;
-
-        if (pre[i] < pmin[i-1]) {
-            pmin[i] = pre[i];
-            pminidx[i] = i;
-        } else {
-            pmin[i] = pmin[i-1];
-            pminidx[i] = pminidx[i-1];
-        }
-    }
-
-    for (int R = d; R <= n; R++) {
-        if (pre[R] >= pmin[R-d]) {
-            int L = pminidx[R-d] + 1;
-            return {L, R};
-        }
-    }
-
-    return {-1, -1};
-}
-
 void solve() {
-    int n, d;
-    cin >> n >> d;
-
-    vector<int> a(n+1);
-    for (int i = 1; i <= n; i++) {
-        cin >> a[i];
-    }
-
-    double L = -1;
-    double R = 101;
-
-    pair<int,int> current, result;
-    for (int i = 0; i < 100; i++) {
-        double M = (L + R) / 2;
-        current = good(n, d, a, M);
-        if (current.first == -1) {
-            R = M;
-        } else {
-            result = current;
-            L = M;
-        }
-    }
-
-    cout << result.first << " " << result.second << endl;
+    
 }
--- exp.cpp
+++ exp.cpp
@@ -12,27 +12,11 @@ int main() {
     cerr << "time: " << (float)clock() / CLOCKS_PER_SEC << endl; return 0;
 }
 
+
+
 void solve() {
-    int n,k;
-    cin>>n>>k;
-    vector<int> a(n), b(n);
-    for(int i=0;i<n;i++) {
-        cin>>a[i];
-    }
-    for(int i=0;i<n;i++) {
-        cin>>b[i];
-    }
-    vector<int> arr;
-    for(int i=0;i<n;i++) {
-        for(int j=0;j<n;j++) {
-            arr.push_back(a[i] + b[j]);
-        }
-    }
-    sort(arr.begin(), arr.end());
-    for(int i=0;i<arr.size();i++) {
-        cout<< i+1 <<" "<<arr[i]<<endl;
-    }
-    for(int i=1;i<26;i++) {
+    cout<<100<<endl;
+    for(int i=1;i<101;i++) {
         cout<<i<<" ";
     }
 }
