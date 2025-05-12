0f1410170df886d980d83f663532ed023a600583
pritam1293 <beherapritam2003@gmail.com>
2025-05-12 23:41:18 +0530

    As of may 12, 2025

--- CF-EDU-TwoP/step2-G.cpp
+++ CF-EDU-TwoP/step2-G.cpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define int long long
 void solve();
-int main() {
+int32_t main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
 #ifndef ONLINE_JUDGE
     freopen("../input.txt", "r", stdin);
@@ -19,5 +20,38 @@ void solve() {
     for(int i=0;i<n;i++) {
         cin>>a[i];
     }
-    
+    deque<pair<int,int>> suffix = {{0,0}};//v
+    deque<pair<int,int>> prefix = {{0,0}};//v1
+
+    int l = 0, ans = INT_MAX;
+    for(int r = 0; r < n; r++) {
+        suffix.push_back({a[r],__gcd(a[r], suffix.back().second)});
+        if(__gcd(prefix.back().second, suffix.back().second) == 1) {
+            ans = min(ans, r-l+1);
+
+            if(prefix.size() == 1) {
+                while(suffix.size() > 1) {
+                    prefix.push_back({suffix.back().first, __gcd(prefix.back().second, suffix.back().first)});
+                    suffix.pop_back();
+                }
+            }
+
+            while(prefix.size() > 1 && __gcd(prefix.back().second, suffix.back().second) == 1) {
+                prefix.pop_back();
+                l++;
+                
+                if(prefix.size() == 1) {
+                    while(suffix.size() > 1) {
+                        prefix.push_back({suffix.back().first, __gcd(prefix.back().second, suffix.back().first)});
+                        suffix.pop_back();
+                    }
+                }
+
+                if(__gcd(suffix.back().second, prefix.back().second) == 1) {
+                    ans = min(ans, r-l+1);
+                }
+            }
+        }
+    }
+    cout<<(ans == INT_MAX ? -1 : ans)<<endl;
 }
--- exp.cpp
+++ exp.cpp
@@ -4,8 +4,8 @@ void solve();
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
 #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
 #endif
     int t=1;
     while(t--)  solve();
@@ -13,11 +13,42 @@ int main() {
 }
 
 void solve() {
-    for(int j = 0; j < 8; j++) {
-        int bit1 = (1 & (j >> 0));
-        int bit2 = (1 & (j >> 1));
-        int bit3 = (1 & (j >> 2));
-        cout<<bit1<<bit2<<bit3<<endl;
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) {
+        cin>>a[i];
     }
+    deque<pair<int,int>> suffix = {{0,0}};//v
+    deque<pair<int,int>> prefix = {{0,0}};//v1
 
+    int l = 0, ans = INT_MAX;
+    for(int r = 0; r < n; r++) {
+        suffix.push_back({a[r],__gcd(a[r], suffix.back().second)});
+        if(__gcd(prefix.back().second, suffix.back().second) == 1) {
+            ans = min(ans, r-l+1);
+            if(prefix.size() == 1) {
+                while(suffix.size() > 1) {
+                    prefix.push_back({suffix.back().first, __gcd(prefix.back().second, suffix.back().first)});
+                    suffix.pop_back();
+                }
+            }
+
+            while(prefix.size() > 1 && __gcd(prefix.back().second, suffix.back().second) == 1) {
+                prefix.pop_back();
+                l++;
+                if(prefix.size() == 1) {
+                    while(suffix.size() > 1) {
+                        prefix.push_back({suffix.back().first, __gcd(prefix.back().second, suffix.back().first)});
+                        suffix.pop_back();
+                    }
+                }
+
+                if(__gcd(suffix.back().second, prefix.back().second) == 1) {
+                    ans = min(ans, r-l+1);
+                }
+            }
+        }
+    }
+    cout<<(ans == INT_MAX ? -1 : ans)<<endl;
 }
