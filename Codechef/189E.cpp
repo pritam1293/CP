8d2d52b1a0032234bc803e68fd2394a494a9b027
pritam1293 <beherapritam2003@gmail.com>
2025-06-06 17:03:31 +0530

    As of june 6, 2025

--- Codechef/189E.cpp
+++ Codechef/189E.cpp
@@ -5,8 +5,8 @@ void solve();
 int32_t main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
 #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
 #endif
     int t=1;
     cin>>t;
@@ -45,4 +45,4 @@ void solve() {
         cout<<ans[i]<<" ";
     }
     cout<<endl;
-}       
+}
