a95215f951aa132614ca634fda20d73852496ea0
pritam1293 <beherapritam2003@gmail.com>
2025-06-10 15:09:02 +0530

    As of jun 10, 2025

---
+++ Codeforces/2117A.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve();
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
+    int t=1;
+    cin>>t;
+    while(t--)  solve();
+    cerr << "time: " << (float)clock() / CLOCKS_PER_SEC << endl; return 0;
+}
+
+void solve() {
+    int n, k;
+    cin>>n>>k;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) {
+        cin>>a[i];
+    }
+    bool one = false;
+    int l = 0;
+    for(int i=0;i<n;i++) {
+        if(a[i] == 1) {
+            one = true;
+            l = i;
+            break;
+        }
+    }
+    int r = n-1;
+    for(int i=n-1;i>=0;i--) {
+        if(a[i] == 1) {
+            r = i;
+            break;
+        }
+    }
+    int cnt = r-l+1;
+    if(one) {
+        if(k >= cnt) {
+            cout<<"YES"<<endl;
+        }
+        else {
+            cout<<"NO"<<endl;
+        }
+    }
+    else {
+        cout<<"YES"<<endl;
+    }
+}
--- code.cpp
+++ code.cpp
@@ -8,37 +8,66 @@ int main() {
     freopen("output.txt", "w", stdout);
 #endif
     int t=1;
+    cin>>t;
     while(t--)  solve();
     cerr << "time: " << (float)clock() / CLOCKS_PER_SEC << endl; return 0;
 }
 
-void solve() {
-    vector<int> a= {3,3,4};
-    int k = 0;
-    long long mod = 1e9 + 7;
-    int n = a.size();
-    vector<int> pre(n+1);
-    pre[0] = 1;
-    int l = 0;
-    int ans = 0;
-    deque<int> dqmin, dqmax;
-    for(int r = 0; r < n; r++) {
-        while(!dqmax.empty() && a[r] > dqmax.back()) dqmax.pop_back();
-        while(!dqmin.empty() && a[r] < dqmin.back()) dqmin.pop_back();
- 
-        dqmax.push_back(a[r]);
-        dqmin.push_back(a[r]);
- 
-        while(!dqmax.empty() && !dqmin.empty() && dqmax.front() - dqmin.front() > k) {
-            if(dqmax.front() == a[l]) dqmax.pop_front();
-            if(dqmin.front() == a[l]) dqmin.pop_front();
-            l++;
-        }
+vector<int> dsu;
+vector<int> minw;
+int parent(int x) {
+    if(dsu[x] < 0) return x;
+    return dsu[x] = parent(dsu[x]);
+}
 
-        long long val = pre[r];
-        if(l > 0) val = (val - pre[l-1] + mod) % mod;
-        pre[r+1] = (pre[r] + val) % mod;
+void merge(int a, int b, int w) {
+    int x = parent(a), y = parent(b);
+    if(x != y) {
+        if(dsu[x] < dsu[y]) {
+            dsu[x] += dsu[y];
+            dsu[y] = x;
+            int temp = minw[x];
+            minw[x] = min({minw[x], minw[y], w});
+            minw[y] = min({temp, minw[y], w});
+        }
+        else {
+            dsu[y] += dsu[x];
+            dsu[x] = y;
+            int temp = minw[y];
+            minw[y] = min({minw[x], minw[y], w});
+            minw[x] = min({minw[x], temp, w});
+        }
     }
-    cout<<(pre[n]-pre[n-1] + mod) % mod<<endl;
+    else {
+        minw[x] = min(minw[x], w);
+    }
+}
+
+void solve() {
+    int n, m;
+    cin>>n>>m;
+    vector<vector<int>> edges(m);
+    for(int i=0;i<m;i++) {
+        int u, v, w;
+        cin>>u>>v>>w;
+        edges[i] = {w, u, v};
+    }
+    sort(edges.begin(), edges.end());
+    dsu = vector<int>(n+1, -1);
+    minw = vector<int>(n+1, 1e9);
+    int ans = INT_MAX;
+    for(auto& edge : edges) {
+        int w = edge[0];
+        int u = edge[1];
+        int v = edge[2];
+        if(parent(u) != parent(v)) {
+            merge(u, v, w);
+        }
+        parent(u);
+        parent(v);
+        if(parent(n) == parent(1)) {
+            ans = min(ans, w + minw[n]);
+        }
+    }
     cout<<ans<<endl;
 }
